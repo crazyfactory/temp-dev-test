d86e832aa9194ae47f1c8425378ce48f44337ae4
freshynice <freshynice@live.com>
2018-12-06 15:18:48 +0700

    script for test 1 - 2 case

--- Test1/test1.php
+++ Test1/test1.php
@@ -8,27 +8,64 @@ output ['d', 1, 2, 3, 'a', 'b', 'c']
 */ 
 
 $input = array(1,2,3,4,5);
-$num = 3;
+$num =  3;
 
 $output = moving($input, $num);
 print_r($output);
+
+echo '<br>------------------<br>';
+
+$input = array('a', 'b', 'c', 'd', 1, 2, 3);
+$num = 10;
+
+$output = moving($input, $num);
+print_r($output);
+
+
 
 function moving($input, $num){
 	$result = array();
 	
 	$count = count($input);
 	
-	for($i=0; $i<$count; $i++){
-		$index = $i - ($num-1);
-		
-		if($index < 0){
-			$newindex = $index+$count;
-			$result[$i] = $input[$newindex];
-		}else{
-		    $newindex = $index;
-			$result[$i] = $input[$newindex];
-		}
-		
+	$diff = $count - $num;
+	
+	
+    for($i=0; $i<$count; $i++){
+        
+    	if($diff >= 0){    
+    	    
+    		$index = $i - ($num-1);
+    		
+    		if($index < 0){
+    			$newindex = $index+$count;
+    			$result[$i] = $input[$newindex];
+    		}else{
+    		    $newindex = $index;
+    			$result[$i] = $input[$newindex];
+    		}
+    		
+    		
+    	}else{ 
+    	    
+    	    $index = $i - ($num-1);
+    		
+    		if($index < 0){
+    		    $newindex = ($index+$count) - ($diff-2);
+    		    $result[$i] = $input[$newindex];
+    		    
+    		    if($newindex >= $count){ 
+    		        $newindex = $newindex - $diff;
+    		        $result[$i] = $input[$newindex-$num];
+    		    }
+    		    
+    		}else{
+    		    $newindex = $index;
+    			$result[$i] = $input[$newindex];
+    		}
+	    
+	    }
+	    
 	}
 
 	return $result;
